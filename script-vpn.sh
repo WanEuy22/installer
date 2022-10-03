dbdab2cf32f6cf247848b20746dca3df3ced3511
WanEuy22 <111190590+WanEuy22@users.noreply.github.com>
2022-10-03 22:23:17 +0700

    Create script-vpn.sh

---
+++ script-vpn.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+GREEN='\033[0;32m'
+NC='\e[0m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+yl='\e[32;1m'
+bl='\e[36;1m'
+gl='\e[32;1m'
+rd='\e[31;1m'
+mg='\e[0;95m'
+blu='\e[34m'
+op='\e[35m'
+or='\033[1;33m'
+bd='\e[1m'
+color1='\e[031;1m'
+color2='\e[34;1m'
+color3='\e[0m'
+#===========================================
+
+clear
+echo -e "======================================"
+read -p "Input your license script : " pwd
+echo -e "======================================"
+
+user=$( curl -s "LINK .TXT" | grep $pwd )
+if [ "$pwd" == "$user" ];
+then
+
+clear
+echo -e "Checking License...."
+sleep 2
+clear
+echo -e "License Verification Success!"
+sleep 2
+clear
+echo -e "Please wait to proces install..." 
+sleep 3
+clear
+rm -rf /root/script-vpn.sh
+wget -O /root/install-sc.sh "https://raw.githubusercontent.com/WanEuy22/main/install-sc.sh" >/dev/null 2>&1
+chmod +x /root/install-sc.sh
+./install-sc.sh
+
+else
+
+clear
+echo -e "Checking License..."
+sleep 2
+clear
+echo -e "License wrong or expired.!!"
+sleep 2
+clear
+echo -e "License Expired or wrong, then the script installation failed.."
+sleep 2
+echo -e "Contact admin to get license.."
+rm -rf /root/script-vpn.sh
+exit
+
+fi
